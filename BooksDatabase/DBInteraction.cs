8f5d02ac58d92ffad6053262b3cddfdd35a6e2ff
DELL-LEONID <lenin0855@gmail.com>
2024-12-06 07:05:18 +0000

    Нandling invalid date in source file

---
+++ BooksDatabase/DBInteraction.cs
@@ -0,0 +1,229 @@
+﻿//using DocumentFormat.OpenXml.Bibliography;
+//using DocumentFormat.OpenXml.InkML;
+//using DocumentFormat.OpenXml.Office2013.Excel;
+//using DocumentFormat.OpenXml.Packaging;
+using Microsoft.EntityFrameworkCore;
+//using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using BooksDatabase;
+using System.ComponentModel;
+
+namespace Books
+{
+    internal class DBInteraction
+    {
+        public static void AddBooksFromCSV(string filePath)
+        {
+            int cellsInFrontOfPublisher = 5;
+            var reader = new StreamReader(filePath);
+            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            csv.Read();
+
+            while (csv.Read())
+            {
+                string[] fields = csv.Parser.Record;
+                string[] filteredFields = fields.Where(field => !string.IsNullOrWhiteSpace(field)).ToArray();
+                string[] briefBookInfo = filteredFields[..(cellsInFrontOfPublisher)];
+
+                for (int i = cellsInFrontOfPublisher; i < filteredFields.Length; i++)
+                {
+                    string[] bookProperties = new string[cellsInFrontOfPublisher + 1];
+                    for (int j = 0; j < briefBookInfo.Length; j++)
+                    {
+                        bookProperties[j] = briefBookInfo[j];
+                    }
+                    bookProperties[^1] = filteredFields[i];
+                    AddNewBook(bookProperties);
+                }
+            }
+        }
+
+        public static void AddNewGenre(string genreName)
+        {
+            BooksDbContext dbContext = new BooksDbContext();
+            Genre newGenre = new Genre
+            {
+                Id = Guid.NewGuid(),
+                Name = genreName
+            };
+            dbContext.Genres.Add(newGenre);
+
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                PrintExceptionMessage(ex);
+            }
+        }
+
+        public static void AddNewAuthor(string authorName)
+        {
+            BooksDbContext dbContext = new BooksDbContext();
+            Author newAuthor = new Author
+            {
+                Id = Guid.NewGuid(),
+                Name = authorName
+            };
+            dbContext.Authors.Add(newAuthor);
+
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                PrintExceptionMessage(ex);
+            }
+        }
+
+        public static void AddNewPublisher(string publisherName)
+        {
+            BooksDbContext dbContext = new BooksDbContext();
+            Publisher newPublisher = new Publisher
+            {
+                Id = Guid.NewGuid(),
+                Name = publisherName
+            };
+            dbContext.Publishers.Add(newPublisher);
+
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                PrintExceptionMessage(ex);
+            }
+        }
+
+        public static void AddNewBook(string[] bookProperties)
+        {
+            try
+            {
+                string title = bookProperties[0];
+                int pages = int.Parse(bookProperties[1]);
+                string genre = bookProperties[2];
+                DateTime releaseDate = Convert.ToDateTime(bookProperties[3]);
+                string author = bookProperties[4];
+                string publisher = bookProperties[5];
+
+                BooksDbContext dbContext = new BooksDbContext();
+
+                Genre existingGenre = new Genre();
+                if (!dbContext.Genres.Any(b => b.Name == genre))
+                {
+                    AddNewGenre(genre);
+                }
+                existingGenre = dbContext.Genres.FirstOrDefault(a => a.Name == genre);
+
+                Author existingAuthor = new Author();
+                if (!dbContext.Authors.Any(b => b.Name == author))
+                {
+                    AddNewAuthor(author);
+                }
+                existingAuthor = dbContext.Authors.FirstOrDefault(a => a.Name == author);
+
+                Publisher existingPublisher = new Publisher();
+                if (!dbContext.Publishers.Any(b => b.Name == publisher))
+                {
+                    AddNewPublisher(publisher);
+                }
+                existingPublisher = dbContext.Publishers.FirstOrDefault(a => a.Name == publisher);
+
+                Book newBook = new Book
+                {
+                    Id = Guid.NewGuid(),
+                    Title = title,
+                    Pages = pages,
+                    GenreID = existingGenre.Id,
+                    AuthorID = existingAuthor.Id,
+                    PublisherID = existingPublisher.Id,
+                    ReleaseDate = releaseDate
+                };
+
+                var existingBook = dbContext.Books.FirstOrDefault(b => b.Title == newBook.Title
+                                                                         && b.Pages == newBook.Pages
+                                                                         && b.GenreID == newBook.GenreID
+                                                                         && b.PublisherID == newBook.PublisherID
+                                                                         && b.AuthorID == newBook.AuthorID
+                                                                         && b.ReleaseDate == newBook.ReleaseDate);
+
+                if (existingBook == null)
+                {
+                    dbContext.Books.Add(newBook);
+                    try
+                    {
+                        dbContext.SaveChanges();
+                    }
+                    catch (Exception ex)
+                    {
+                        PrintExceptionMessage(ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"error while adding entry");
+                Console.WriteLine(ex.ToString());
+            }
+            
+        }
+
+        public static void RemoveFromAuthors(string authorName)
+        {
+            BooksDbContext dbContext = new BooksDbContext();
+            Author existingAuthor = dbContext.Authors.FirstOrDefault(a => a.Name == authorName);
+            dbContext.Authors.Remove(existingAuthor);
+            try
+            {
+                dbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                PrintExceptionMessage(ex);
+            }
+        }
+
+        public static void RemoveFromBooks(string bookName)
+        {
+            BooksDbContext dbContext = new BooksDbContext();
+            Book existingBook = new Book();
+            while (dbContext.Books.Any(b => b.Title == bookName))
+            {
+                existingBook = dbContext.Books.FirstOrDefault(a => a.Title == bookName);
+                dbContext.Books.Remove(existingBook);
+
+                try
+                {
+                    dbContext.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    PrintExceptionMessage(ex);
+                }
+            }
+        }
+
+        public static void PrintExceptionMessage(Exception ex)
+        {
+            Console.WriteLine("Error when saving changes:");
+            Console.WriteLine(ex.Message);
+            if (ex.InnerException != null)
+            {
+                Console.WriteLine("Inner exception details:");
+                Console.WriteLine(ex.InnerException.Message);
+            }
+        }
+    }
+}
--- BooksDatabase/Program.cs
+++ BooksDatabase/Program.cs
@@ -1,10 +1,68 @@
-﻿namespace BooksDatabase
+﻿
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Books
 {
     public class Program
     {
-        public static void Main(string[] args)
-        { 
+        static void Main(string[] args)
+        {
+            string booksBriefCsv = "E:\\booksBrief.csv";
+            string booksCsv = "E:\\books.csv";
+            //string booksXlsx = "E:\\books4.xlsx";
 
+            //Console.WriteLine($"1 - AddBooksFromXLSX {booksXlsx}");
+            Console.WriteLine("2 - RemoveFromBooks");
+            Console.WriteLine("3 - Remove 5 Test Books");
+            Console.WriteLine($"4 - BooksFromCSV {booksBriefCsv} (8 entries)");
+            Console.WriteLine($"5 - BooksFromCSV {booksCsv} (49 entries)");
+
+            int choice = int.Parse(Console.ReadLine());
+            if (choice == 1)
+            {
+                //DBInteraction.AddBooksFromXLMS(booksXlsx);
+            }
+            else if (choice == 2)
+            {
+                while (true)
+                {
+                    Console.WriteLine("Enter book name:");
+                    string name = Console.ReadLine();
+                    if (name == "exit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        DBInteraction.RemoveFromBooks(name);
+                    }
+                }
+            }
+            else if (choice == 3)
+            {
+                DBInteraction.RemoveFromBooks("To Kill a Mockingbird");
+                DBInteraction.RemoveFromBooks("1984");
+                DBInteraction.RemoveFromBooks("The Great Gatsby");
+                DBInteraction.RemoveFromBooks("Jane Eyre");
+                DBInteraction.RemoveFromBooks("The Picture of Dorian Gray");
+            }
+            else if (choice == 4)
+            {
+                DBInteraction.AddBooksFromCSV(booksBriefCsv);
+            }
+            else if (choice == 5)
+            {
+                DBInteraction.AddBooksFromCSV(booksCsv);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
         }
     }
 }
